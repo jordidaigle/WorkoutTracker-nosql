1688a7c4768dcd9ede8bd55d5fce860329fcf42f
jordidaigle <jdaigle992@gmail.com>
2020-08-06 18:55:34 -0500

    npm installs

---
+++ Develop/public/models/workout.js
@@ -0,0 +1,49 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+const schemaWorkout=new Schema({
+
+    Day: {
+        type: Date,
+        default: Date.now
+    };
+
+    Exercises: [{
+        Name: {
+            type: String,
+            trim: true,
+            required: "Name is Required"
+        },
+
+        type: {
+            type: String,
+            required: "Type of exercise is Required"
+        },
+        weight: {
+            type: Number,
+            required: "Weight is Required (even if 0)"
+        },
+        set: {
+            type: Number,
+            required: "Number of sets is Required"
+        },
+        reps: {
+            type: Number,
+            required: "Number of reps is Required"
+        },
+        duration: {
+            type: Number,
+            required: "Duration is Required"
+        },
+        distance: {
+            type: Number,
+            required: "Distance is Required"
+        },
+    }]
+});
+
+// I should also be able to track the name, type, weight, sets, reps, and duration of exercise.
+
+const Workout=mongoose.model("Workout",schemaWorkout);
+
+module.exports=Workout;
---
+++ Develop/server.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const express = require('express');
+const logger = require('morgan');
+
+// Establish connection with MongoDb
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExample", { useNewUrlParser: true });
+
+// Declare PORT
+const PORT = process.env.PORT || 8000;
+
+// Inititalize Express
+const app = express();
+
+// Middleware
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Declaring static folders
+app.use(express.static("public"));
+
+// Require Routes
+// app.use(require("./routes/api.js"));
+// app.use(require("./routes/views.js"));
+
+// Init PORT listener
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});

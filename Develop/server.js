9825db916b88b08602dfe312f51b57eec363a92c
jordidaigle <jdaigle992@gmail.com>
2020-08-06 20:29:46 -0500

    Pushed in my APi routes

--- Develop/server.js
+++ Develop/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Require Routes
-// app.use(require("./routes/api.js"));
+app.use(require("./routes/api.js"));
 app.use(require("./routes/views.js"));
 
 // Init PORT listener

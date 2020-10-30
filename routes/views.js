07284af32b56663af990951bcf695ada09edd9ef
jordidaigle <jdaigle992@gmail.com>
2020-10-30 13:29:51 -0500

    Restructured files a bit

--- routes/views.js
+++ routes/views.js
@@ -6,19 +6,19 @@ let router = require('express').Router();
   
     // index route loads view.html
     router.get("/", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/index.html"));
+      res.sendFile(path.join(__dirname, "../public/View/index.html"));
     });
   
     // add route loads the add.html page,
     // where users can enter new characters to the db
     router.get("/exercise", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/exercise.html"));
+      res.sendFile(path.join(__dirname, "../public/View/exercise.html"));
     });
   
     // all route loads the all.html page,
     // where all characters in the db are displayed
     router.get("/stats", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/stats.html"));
+      res.sendFile(path.join(__dirname, "../public/View/stats.html"));
     });
 
   module.exports = router;
